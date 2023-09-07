5dadb073ab6c34bf65baa75accadadbd2b057ba3
Szabó Gergő <gergo85@gmail.com>
2023-09-07 15:01:12 +0200

    Create Hungarian language

---
+++ resources/lang/hu/filament-firewall.php
@@ -0,0 +1,27 @@
+<?php 
+
+return [
+    'filament.resource.ip.navigationLabel' => 'IP tűzfal',
+    'filament.resource.ip.getNavigationGroup' => 'Rendszer',
+    'filament.resource.ip.label' => 'IP cím',
+    'filament.resource.ip.modalLabel' => 'IP cím',
+    'filament.resource.ip.pluralLabel' => 'IP cím',
+    'filament.resource.ip.pluralModelLabel' => 'IP tűzfal',
+
+    'table.column.ip' => 'IP cím',
+    'table.column.prefix_size' => 'Előtag',
+    'table.column.blocked' => 'Blokkolva?',
+    'table.column.is_allow' => 'Művelet',
+
+    'form.field.ip' => 'IP cím',
+    'form.field.prefix_size' => 'Előtag',
+    'form.field.blocked' => 'Blokkolva?',
+    'form.field.is_allow' => 'Művelet',
+
+    'action.addMyIp' => 'IP címem hozzáadása',
+    'action.addMyIp.alreadyAdded' => 'Sikeresen hozzáadva.',
+    'action.fillMyIp' => 'IP címem kitöltése',
+
+    'labels.allow' => 'Engedélyezés',
+    'labels.deny' => 'Tiltás',
+];
