08ea238c73348cc566b7982d948cfa3d0b25af1e
a21ns1g4ts <atila.danvi@outlook.com>
2024-09-21 14:33:26 -0300

    Add pt translations

---
+++ resources/lang/pt_BR/filament-firewall.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'filament.resource.ip.navigationLabel' => 'Firewall IP',
+    'filament.resource.ip.getNavigationGroup' => 'Sistema',
+    'filament.resource.ip.label' => 'Endereço IP',
+    'filament.resource.ip.modalLabel' => 'Endereço IP',
+    'filament.resource.ip.pluralLabel' => 'Endereços IP',
+    'filament.resource.ip.pluralModelLabel' => 'Firewall IP',
+
+    'table.column.ip' => 'Endereço IP',
+    'table.column.prefix_size' => 'Prefixo',
+    'table.column.blocked' => 'Bloqueado?',
+    'table.column.is_allow' => 'Ação',
+    'table.column.created_at' => 'Criado em',
+    'table.column.updated_at' => 'Atualizado em',
+
+    'form.field.ip' => 'Endereço IP',
+    'form.field.prefix_size' => 'Prefixo',
+    'form.field.blocked' => 'Bloqueado?',
+    'form.field.is_allow' => 'Ação',
+
+    'action.addMyIp' => 'Adicionar meu IP',
+    'action.addMyIp.alreadyAdded' => 'Adicionado com sucesso.',
+    'action.fillMyIp' => 'Preencher meu IP',
+
+    'labels.allow' => 'Permitir',
+    'labels.deny' => 'Negar',
+];
