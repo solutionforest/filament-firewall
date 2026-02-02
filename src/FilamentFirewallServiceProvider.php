4935226be4ed600b9ef6ec034f5fc3ec19f034f1
cklei-carly <carly.cheung@solutionforest.net>
2026-02-02 17:56:06 +0800

    Handle vendor:publish failures in installer
    Wrap the vendor:publish call in a try/catch during the package install flow. If publishing Akaunting Firewall resources fails, the installer now shows an error, prints a suggested manual command (php artisan vendor:publish --tag=firewall) and aborts with exit(1). This provides clearer guidance when the dependent package isn't available instead of letting the exception bubble up.

--- src/FilamentFirewallServiceProvider.php
+++ src/FilamentFirewallServiceProvider.php
@@ -23,9 +23,19 @@ class FilamentFirewallServiceProvider extends PackageServiceProvider
                     ->publishConfigFile()
                     ->publishMigrations()
                     ->startWith(function (InstallCommand $command) {
-                        $command->call('vendor:publish', [
-                            '--tag' => 'firewall',   // Required package
-                        ]);
+                        try {
+                            $command->call('vendor:publish', [
+                                '--tag' => 'firewall',   // Required package
+                                ]);
+                        } catch (\Throwable $th) {
+                            // display error message
+                            $installCommand = 'php artisan vendor:publish --tag=firewall';
+                            $command->error('Failed to publish Akaunting Firewall resources. Please make sure Akaunting Firewall is installed.');
+                            // display instruction to user
+                            $command->info("Please run the following command manually: \n  $installCommand");
+                            // stop execution
+                            exit(1);
+                        }
                     })
                     ->endWith(function (InstallCommand $command) {
                         // run migration
